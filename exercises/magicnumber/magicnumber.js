5ca3b99e50daa28c614f6a35f2e868c17c0d664b
aitorarias <aitor.acruz@gmail.com>
2019-01-21 18:25:29 +0100

    added magic number challenge

---
+++ exercises/magicnumber/magicnumber.js
@@ -0,0 +1,19 @@
+let max = 20;
+let min = 1; //
+let random = Math.random() * (max - min) + min;
+random = parseInt(random);
+
+while (true) {
+  let user = prompt("Pon un número");
+
+  if (user == random) {
+    alert("¡Has acertado! 🎉");
+    break;
+  } else if (user == 0) {
+    break;
+  } else if (user < random) {
+    alert("El numero es menor que el numero magico. Inténtalo de nuevo 🕳️");
+  } else if (user > random) {
+    alert("EL numero es mayor que el numero magico Inténtalo de nuevo 🕳️");
+  }
+}
